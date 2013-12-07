eb26d8a8ecf2980f6a9be55f8a3c319fa5b3f7c8
Eric Time <windwhinny@gmail.com>
2013-12-07 11:51:44 +0800

    init

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express=require('express');
-var config=require('config');
-var app=express.app();
+var config=require('./config');
+var app=express();
 
+// use some middleware
+app.use(express.bodyParser());
+app.use(express.compress());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.static('public'))
+
+app.get('/',function(req,res,next){
+  if(req.accepts('html')){
+    res.sendfile('./public/index.html',function(err){
+      next(err);
+    }) 
+  }else{
+    next();
+  }
+})
 app.listen(config.port);
 console.log('Server start at port '+config.port);
