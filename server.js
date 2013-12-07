00415abb75ba0c81aa13b9cb15dbee0916710766
windwhinny <windwhinny@gmail.com>
2013-12-07 14:22:26 +0800

    ..

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express=require('express');
 var config=require('./config');
+var request=require('request');
+var MongoClient=require('mongodb').MongoClient;
+var MongoServer=require('mongodb').Server;
 var app=express();
 
 // use some middleware
@@ -13,10 +16,105 @@ app.get('/',function(req,res,next){
   if(req.accepts('html')){
     res.sendfile('./public/index.html',function(err){
       next(err);
-    }) 
+    })
   }else{
     next();
   }
 })
-app.listen(config.port);
-console.log('Server start at port '+config.port);
+
+var db=null;
+var mongodbClient=new MongoClient(new MongoServer('localhost',27017));
+function init(done){
+	mongodbClient.open(function(err,mongodbClient){
+		mongodbClient
+			.db('mining')
+			.collection('record', function(err, collection) {
+				db=collection;
+				if(!db){
+					return done(new Error('can not connect to databse'));
+					startFetch();
+				}
+			});
+	})
+	app.listen(config.port);
+	console.log('Server start at port '+config.port);
+}
+var addToSaveQuery=(function(){
+	var query=[];
+	var queuing=false;
+	function startQueuing(){
+		if(queuing)return;
+		queuing=true;
+		function next(){
+			if(query.length){
+				var task=query.shift();
+				task.beforeSave(task.data,function(err,data){
+					if(err){
+						throw err;
+						return
+					}
+					task.data=data;
+					db.save(task.data,task.done);
+				})
+				next();
+			}
+		}
+		next();
+	}
+	return function(data,beforeSave,done){
+		data.date=Date();
+		query.push({
+			data:data,
+			beforeSave:beforeSave,
+			done:done
+		});
+		startQueuing();
+	}
+})();
+function saveRecord(data){
+	var totaleReward=data.confirmed_reward+data.unconfirm_reward;
+	data.total_reward=totaleReward;
+	addToSaveQuery(data,function(data,done){
+		db.findOne({},{
+			limit:1,
+			sort:['date']
+		},function(err,doc){
+			if(err)return done(err);
+			data.new_reward=data.total_reward-doc.total_reward;
+			done(err,data);
+		})
+	},function(err,data){
+		if(err){
+			throw err;
+			return;
+		}
+		console.log(data);
+	})
+}
+function startFetch(){
+	setInterval(function() {
+		var url='http://bbq.ltcoin.net/api.php?key=966989b42b5cbe8378e9e50c6500710c';
+		request(url,function(err,res,data){
+			if(err){
+				throw err;
+				return;
+			}
+			try{
+				var data=JSON.parse(data);
+				console.log(data);
+				saveRecord(data);
+			}catch (err){
+				throw err;
+				return
+			}
+		})
+	},1000);
+}
+
+init(function(err){
+	if(err){
+		console.error(err);
+		process.exit();
+	}
+});
+
